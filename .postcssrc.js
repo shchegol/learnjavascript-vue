d4a24aba4b99b589cdb91bbc6117b11866e7895e
Alexander Shchegol <zelenzoom@gmail.com>
2018-01-30 20:37:47 +0300

    Task 3 / webpack setup

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+n// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    "postcss-import": {},
+    "postcss-url": {},
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {}
+  }
+}
